c9d6c868df209353a676f5b11ca128640fbe1111
Space Captin <thehamburgalar1@gmail.com>
2018-01-23 05:35:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ if (name === "Susan") {
       console.log("Expected the name to be Susan")
 }
 
+
+if (height === "27") {
+  console.log("the height is correct,son.")
+} else {
+      consle.log("Expected the height to be 27")
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
